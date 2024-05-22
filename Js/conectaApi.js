fcdc8e839c3dbdc7ce2bfa82d29ab7d836efff22
felipe melo <luizfelipemelo001@gmail.com>
2024-05-22 11:04:11 -0300

    :sparkles: Feat: post  produtos lista

--- Js/conectaApi.js
+++ Js/conectaApi.js
@@ -4,6 +4,23 @@ async function listaProdutos() {
 	return conecaoConvertida;
 }
 
+async function criaProdudo(nome, imagem, valor) {
+	const conexao = await fetch("http://localhost:3000/produtos", {
+		method: "POST",
+		headers: {
+			"Content-type": "application/json",
+		},
+		body: JSON.stringify({
+			nome: nome,
+			imagem: imagem,
+			valor: `R$${valor},00`,
+		}),
+	});
+	const conecaoConvertida = await conexao.json();
+	return conecaoConvertida;
+}
+
 export const conectaApi = {
 	listaProdutos,
+	criaProdudo,
 };
